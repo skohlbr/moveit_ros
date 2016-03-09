2339cc335ccca82bb629e25fb7c913cc12636562
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-03-09 16:53:37 +0100

    Revert "Make octomap_monitor public."
    This reverts commit 5d7c314529d36ad967f5795b1fc5090cc34a6456.

--- planning/planning_scene_monitor/include/moveit/planning_scene_monitor/planning_scene_monitor.h
+++ planning/planning_scene_monitor/include/moveit/planning_scene_monitor/planning_scene_monitor.h
@@ -350,9 +350,6 @@ public:
 
   void clearOctomap();
 
-  // include a octomap monitor
-  boost::scoped_ptr<occupancy_map_monitor::OccupancyMapMonitor> octomap_monitor_;
-
 protected:
 
   /** @brief Initialize the planning scene monitor
@@ -443,6 +440,9 @@ protected:
 
   boost::scoped_ptr<message_filters::Subscriber<moveit_msgs::CollisionObject> > collision_object_subscriber_;
   boost::scoped_ptr<tf::MessageFilter<moveit_msgs::CollisionObject> > collision_object_filter_;
+
+  // include a octomap monitor
+  boost::scoped_ptr<occupancy_map_monitor::OccupancyMapMonitor> octomap_monitor_;
 
   // include a current state monitor
   CurrentStateMonitorPtr current_state_monitor_;
